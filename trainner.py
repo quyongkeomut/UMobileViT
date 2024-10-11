d0e2e832445379e47ffda47d01cdf9d252ffcc55
bangthe2222 <bangthedz2222@gmail.com>
2024-10-11 22:48:28 +0700

    add trainner.py

---
+++ trainner.py
@@ -0,0 +1,12 @@
+import torch
+import tqdm
+from torch.utils.data import DataLoader
+from datasets.bdd_datasets import BDD100KDataset
+from loss.loss import TotalLoss
+
+class Trainer:
+    def __init__(self) -> None:
+        pass
+
+    def run(self):
+        pass
