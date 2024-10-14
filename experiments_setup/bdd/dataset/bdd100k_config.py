fba3db61ea7cb165bdf6e0eb9aa720efc8688e0b
bangthe2222 <bangthedz2222@gmail.com>
2024-10-14 17:33:49 +0700

    change setting

--- experiments_setup/bdd/dataset/bdd100k_config.py
+++ experiments_setup/bdd/dataset/bdd100k_config.py
@@ -1,9 +1,9 @@
 alpha: float =  0.5
 out_channel = [2, 2]
 patch_size = (2,2)
-num_epochs: int = 100
-batch_size: int = 2
-optimizer: str = "adam"
+num_epochs: int = 50
+batch_size: int = 24
+optimizer: str = "adamw"
 lr: float = 5e-4
 momentum: float = 0.9
 nesterov: bool = False
