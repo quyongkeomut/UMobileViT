ee8c88332435b8af88f6c8168f608c0e671c7070
Phuoc Thinh <thinhnguyen.7062@gmail.com>
2024-10-19 14:00:33 +0700

    change base learning rate to 1e-3

--- experiments_setup/bdd/configs/bdd100k_experiment_config.py
+++ experiments_setup/bdd/configs/bdd100k_experiment_config.py
@@ -2,7 +2,5 @@ OUT_CHANNELS = [2, 2]
 PATCH_SIZE = (2, 2)
 OPTIMIZER_NAME: str = "adamw"
 OPTIM_ARGS = {
-    "lr": 5e-4,
-    # "momentum": momentum,
-    # "nesterov": nesterov 
+    "lr": 1e-3,
 }
