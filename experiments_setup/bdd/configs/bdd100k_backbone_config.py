f8de51d719bde1ce10365d1383d45df1eb49c99d
Phuoc Thinh <thinhnguyen.7062@gmail.com>
2024-10-29 23:12:04 +0700

    change group normalization to layer normalization, and set num_transformer_block to 1

--- experiments_setup/bdd/configs/bdd100k_backbone_config.py
+++ experiments_setup/bdd/configs/bdd100k_backbone_config.py
@@ -3,9 +3,9 @@ BACKBONE_CONFIGS = {
     "d_model": 64,
     "expansion_factor": 8,
     "dropout_p": 0.1,
-    "norm_num_groups": 4,
+    "norm_num_groups": 1,   
     "bias": True, 
-    "num_transformer_block": 2,
+    "num_transformer_block": 1,
     "initializer": "he_uniform",
     "dtype": None
 }
