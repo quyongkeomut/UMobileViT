cdfbcf13007b9582fb981401e61c4f33318ef6fe
Phuoc Thinh <thinhnguyen.7062@gmail.com>
2024-10-29 23:27:47 +0700

    re-arrange code.

--- GFlosps_test.py
+++ GFlosps_test.py
@@ -16,69 +16,67 @@ from experiments_setup.bdd.configs.bdd100k_backbone_config import BACKBONE_CONFI
 import time
 
 if __name__ == "__main__":
-    model = UMobileViT(alpha=0.25, 
-                       patch_size=(2, 2), 
-                       out_channels=[2,2],
-                       device="cuda",
-                       **BACKBONE_CONFIGS
-                       )
-    model = model.to("cuda")
-    check_point = torch.load(r"weights\scale_0.25\2024_10_20_13_42\last.pth", weights_only=True)
-        
-    model.load_state_dict(check_point["model_state_dict"])
-    # model.load_state_dict(torch.load(r"test_model.pth", weights_only=True))
-    img_size = (3, 320, 640)
-    input = torch.randn(size=(1, *img_size))
-    input_shape = (1, *img_size)
+    #
+    # init model
+    #
+    model = UMobileViT(
+        alpha=0.25, 
+        patch_size=(2, 2), 
+        out_channels=[2,2],
+        device="cuda",
+        **BACKBONE_CONFIGS
+    )
+    model.eval()
     
-    model_inputs = (input.to("cuda"), )
+    # load weights from checkpoint
+    # check_point = torch.load(r"weights\scale_0.25\2024_10_20_13_42\last.pth", weights_only=True)
+    # model.load_state_dict(check_point["model_state_dict"])
+    
+    
+    #
+    # create dummy data for profiling
+    # 
+    img_size = (3, 320, 640)
+    img_shape = (1, *img_size)
+    input = torch.randn(size=(1, *img_size), device="cuda")
+    model_inputs = (input, )
 
+
+    #
+    # check model fps
+    #
     with torch.inference_mode():
+        fps_list = []
         while True:
             t1 = time.time()
             
-            output = model(input.to("cuda"))
-        
-            print("FPS:", 1/(time.time() - t1))
-        
-    # fvcore
-    # flops = fnn.FlopCountAnalysis(model, model_inputs)
-    # print(fnn.flop_count_table(flops, max_depth=3))
+            output = model(input)
+
+            t: float = 1/(time.time() - t1)
+            print("FPS:", t)
+            fps_list.append(t)
+            if len(fps_list) == 1000: 
+                import statistics
+                print("avg FPS:", statistics.mean(fps_list))
+                print("std FPS:", statistics.stdev(fps_list))
+                break
+            
     
-    
-    # torch summary
+    #
+    # check model parameters by torchinfo
+    #
     # summary(model, input_data=model_inputs, depth=5)
 
 
     # pytorch op counter - thop
-    macs, params = profile(model, inputs=model_inputs)
-    macs, params = clever_format([macs*2, params], "%.3f")
-    print(macs, params)
+    # macs, params = profile(model, inputs=model_inputs)
+    # macs, params = clever_format([macs*2, params], "%.3f")
+    # print(macs, params)
     
     
-    # flops counter - ptflops
-    # macs, params = get_model_complexity_info(model, img_size, as_strings=True, backend='pytorch',
-    #                                          print_per_layer_stat=True, verbose=True)
-    # print(f'Computational complexity: {macs}')
-    # print(f'Number of parameters: {params}')
-    
-    
-    # torch profiler
+    # torch profiler 
     # with profile(activities=[ProfilerActivity.CPU], record_shapes=True, with_flops=True) as prof:
     #     with record_function("model_inference"):
     #         model(input)
     # print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
     
-    
-    # --segmentation head---
-    # model = SegmentationHead(
-    #     in_channels= 48,
-    #     out_channels= 2
-    # )
-# 
-    # input = torch.randn(1, 48, 72, 128)
-# 
-    # model_inputs = (input.to("cuda"), )
-    # flops = fnn.FlopCountAnalysis(model, model_inputs)
-    # print(fnn.flop_count_table(flops, max_depth=5))
-    # summary(model, input_data=input.to("cuda"), depth=5)
