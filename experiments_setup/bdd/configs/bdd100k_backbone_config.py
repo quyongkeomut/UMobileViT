cf5a1da2e03492d4141ef556d7d435fe1e8eaf61
Phuoc Thinh <thinhnguyen.7062@gmail.com>
2024-11-01 23:17:44 +0700

    change base backbone config

--- experiments_setup/bdd/configs/bdd100k_backbone_config.py
+++ experiments_setup/bdd/configs/bdd100k_backbone_config.py
@@ -1,11 +1,11 @@
 BACKBONE_CONFIGS = {
     "in_channels": 3,
     "d_model": 64,
-    "expansion_factor": 8,
+    "expansion_factor": 6,
     "dropout_p": 0.1,
     "norm_num_groups": 1,   
     "bias": True, 
-    "num_transformer_block": 1,
+    "num_transformer_block": 2,
     "initializer": "he_uniform",
     "dtype": None
 }
