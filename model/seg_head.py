a70ddf72f1db18a125bb0753086f518063dff332
Phuoc Thinh <thinhnguyen.7062@gmail.com>
2024-11-11 17:55:24 +0700

    change the architecture of segmentation head: Each layer of segmentation head is the additive decoder layer, which receive the feature maps from step block.

--- model/seg_head.py
+++ model/seg_head.py
@@ -1,14 +1,13 @@
 from typing import Tuple, Callable
 
 import torch
-from torch import Tensor
+from torch import Tensor, cat
 from torch.nn import (
     Module,
     Sequential,
+    ModuleList,
     ReLU,
     Conv2d,
-    GroupNorm,
-    Upsample,
 )
 from torch.nn.modules.utils import _pair
 
@@ -16,205 +15,261 @@ from torch.nn.init import zeros_
 
 from model.module import (
     _get_initializer,
-    _get_clones
+)
+from model.decoder import (
+    UMobileViTDecoderAdditiveLayer,
+    _get_upsample_block
 )
 
 
-def _get_upsample_block(
-    in_channels: int,
-    bias: bool,
-    norm_num_groups: int,
-    initializer: Callable,
-    **kwargs,
-) -> Sequential:
-    # depthwise conv, followed by non-linearity and group norm
-    block = Sequential(
-        Upsample(
-            scale_factor=kwargs["scale_factor"], 
-            mode=kwargs["mode"]
-        ),
-        Conv2d(
-            in_channels=in_channels,
-            out_channels=in_channels,
-            kernel_size=3,
-            stride=1,
-            padding=(1, 1),
-            groups=in_channels,
-            bias=bias,
-            device=kwargs["device"],
-            dtype=kwargs["dtype"]
-        ),
-        ReLU(),
-        GroupNorm(
-            num_groups=norm_num_groups,
-            num_channels=in_channels,
-            device=kwargs["device"],
-            dtype=kwargs["dtype"]
-        ),
-    )
-    
-    initializer(block[1].weight)
-    if block[1].bias is not None:
-        zeros_(block[1].bias)
-    
-    return block
+class UpsampleHead(Module):
+    def __init__(
+        self,  
+        d_model: int = 64,
+        expansion_factor: float = 3,
+        dropout_p: float = 0.1,
+        norm_num_groups: int = 4,
+        bias: bool = True, 
+        initializer: str | Callable[[Tensor], Tensor] = "he_uniform",
+        device=None,
+        dtype=None,
+        **kwargs,
+    ) -> None:
+        r"""
+        Upsample Head is the module that restore the resolution similar to the original input.
 
-
-def _get_upsample_head(
-    in_channels: int,
-    new_space_dim: int,
-    bias: bool,
-    norm_num_groups: int,
-    initializer,
-    **kwargs,
-) -> Sequential:
+        Args:
+            d_model (int, optional): Dimentions / Number of feature maps of the whole model. 
+                Defaults to 64.
+            expansion_factor (float, optional): Expansion factor in Inverted Residual block. 
+                Defaults to 3.
+            dropout_p (float, optional): Dropout probability. Defaults to 0.1.
+            norm_num_groups (int, optional): Control number of groups to be normalized. If
+                ``norm_num_groups`` = 1, this is equivalent to LayerNorm. Defaults to 4.
+            bias (bool, optional): If ``True``, add trainable bias to building blocks. Defaults to True.
+            initializer (str | Callable[[Tensor], Tensor], optional): Parameters initializer.
+                Defaults to "he_uniform" aka _kaiming_uniform.
+        """
+        super().__init__()
+        self.initializer = _get_initializer(initializer)
+        
+        factory_kwargs = {"device": device, "dtype": dtype}
+        decoder_layer_kwargs = {
+            "in_channels": d_model,
+            "expansion_factor": expansion_factor,
+            "dropout_p": dropout_p,
+            "norm_num_groups": norm_num_groups,
+            "bias": bias,
+            "initializer": initializer,
+        }
+        upsampling_kwargs = {
+            "scale_factor": 2,
+            "mode": "nearest"
+        }
+        upsampling_conv_kwargs = {
+            "in_channels": d_model,
+            "out_channels": d_model,
+            "kernel_size": 3,
+            "stride": 1,
+            "padding": (1, 1),
+            "groups": d_model,
+            "norm_num_groups": norm_num_groups,
+            "bias": bias,
+        }
+        
+        upsample_head_x2 = ModuleList([
+            _get_upsample_block(
+                self.initializer,
+                **upsampling_kwargs, 
+                **upsampling_conv_kwargs,
+                **factory_kwargs
+            ),
+            UMobileViTDecoderAdditiveLayer(
+                **decoder_layer_kwargs, 
+                **factory_kwargs,
+                **kwargs,
+            )
+        ])
+        
+        upsample_head_x4 = ModuleList([
+            _get_upsample_block(
+                self.initializer,
+                **upsampling_kwargs, 
+                **upsampling_conv_kwargs,
+                **factory_kwargs
+            ),
+            UMobileViTDecoderAdditiveLayer(
+                **decoder_layer_kwargs, 
+                **factory_kwargs,
+                **kwargs,)
+        ])
+        
+        upsample_head_x8 = ModuleList([
+            _get_upsample_block(
+                self.initializer,
+                **upsampling_kwargs, 
+                **upsampling_conv_kwargs,
+                **factory_kwargs
+            ),
+        ])
+        
+        self.layers = ModuleList([
+            upsample_head_x2,
+            upsample_head_x4, 
+            upsample_head_x8,
+        ])
+        
     
-    
-    factory_kwargs = {"device": kwargs["device"], "dtype": kwargs["dtype"]}
-    upsampling_kwargs = {"scale_factor": kwargs["scale_factor"], "mode": kwargs["mode"]}
-    
-    # init the upsample part, which share the same structure at each head
-    upsample_head = Sequential(
-        # project to new space
-        Conv2d(
-            in_channels=in_channels,
-            out_channels=new_space_dim,
-            kernel_size=1,
-            bias=bias,
-            **factory_kwargs
-        ),
-        ReLU(),
-        GroupNorm(
-            num_groups=norm_num_groups,
-            num_channels=new_space_dim,
-            **factory_kwargs
-        ),
-    )
-    
-    # upsize to 8 times 
-    upsample_head.extend(
-        _get_clones(
-            _get_upsample_block, 
-            N=3,
-            in_channels=new_space_dim,
-            bias=bias,
-            norm_num_groups=norm_num_groups,
-            initializer=initializer,
-            **upsampling_kwargs, 
-            **factory_kwargs
-        )
-    )
-    return upsample_head
-
+    def forward(
+        self,
+        input: Tensor,
+        outputs_stem: Tuple[Tensor]
+    ) -> Tensor:
+        assert (
+            len(outputs_stem) == len(self.layers) - 1
+        ), f"length of output_stems must be {len(self.layers) - 1}, got {len(outputs_stem)}"
+        
+        Z = input
+        for i, output_stem in enumerate(outputs_stem):
+            Z = self.layers[i][0](Z) # upsample
+            for layer in self.layers[i][1:]: # additive layer
+                Z = layer(Z, output_stem)
+        Z = self.layers[-1][0](Z) # just upsample
+        
+        return Z
+        
 
 class SegmentationHead(Module):
     def __init__(self, 
-        d_model: int = 96,
-        alpha: float = 1,
+        d_model: int,
+        expansion_factor: float = 3,
+        dropout_p: float = 0.1,
         out_channels: int | Tuple[int, int] = [2, 2],
         norm_num_groups: int = 4,
         bias: bool = True, 
         initializer: str | Callable[[Tensor], Tensor] = "he_uniform",
         device=None,
         dtype=None,
-        **kwargs
+        **kwargs,
     ) -> None:
-        
+        r"""
+        Upsample Head is the module that generate segmentation mask for Line and Drivable area
+        Segmentation task.
+
+        Args:
+            d_model (int): Dimentions / Number of feature maps of the whole model. 
+            expansion_factor (float, optional): Expansion factor in Inverted Residual block.
+                Defaults to 3.
+            dropout_p (float, optional): Dropout probability. Defaults to 0.1.
+            out_channels (int | Tuple[int, int], optional): Number of channels of output massk. 
+                Defaults to (2, 2).
+            norm_num_groups (int, optional): Control number of groups to be normalized. If
+                ``norm_num_groups`` = 1, this is equivalent to LayerNorm. Defaults to 4.
+            bias (bool, optional): If ``True``, add trainable bias to building blocks. Defaults to 
+                True.
+            initializer (str | Callable[[Tensor], Tensor], optional): Parameters initializer.
+                Defaults to "he_uniform" aka _kaiming_uniform.
+        """
         super().__init__()
         
-        upsampling_kwargs = {"scale_factor": 2, "mode": "nearest"}
         factory_kwargs = {"device": device, "dtype": dtype}
         initializer = _get_initializer(initializer)
         self.initializer = initializer
-        in_channels: int = int(alpha*d_model)
-        new_space_dim = in_channels
         out_channels = _pair(out_channels)
         
-        self.lane_head = _get_upsample_head(
-            in_channels=in_channels,
-            new_space_dim=new_space_dim,
-            bias=bias,
-            norm_num_groups=norm_num_groups,
-            initializer=self.initializer,
-            **factory_kwargs,
-            **upsampling_kwargs
-        )
-        self.drivable_head = _get_upsample_head(
-            in_channels=in_channels,
-            new_space_dim=new_space_dim,
-            bias=bias,
-            norm_num_groups=norm_num_groups,
-            initializer=self.initializer,
-            **factory_kwargs,
-            **upsampling_kwargs
-        )
-        
-        # lane head
-        self.lane_head.extend([
-            # output conv
-            Conv2d(
-                in_channels=new_space_dim,
-                out_channels=new_space_dim,
-                kernel_size=3,
-                stride=1,
-                padding=(1, 1),
-                groups=new_space_dim,
+        self.lane_head = ModuleList([
+            UpsampleHead(
+                d_model=d_model,
+                expansion_factor=expansion_factor,
+                dropout_p=dropout_p,
+                norm_num_groups=norm_num_groups,
                 bias=bias,
-                **factory_kwargs,
-            ),
-            ReLU(),
-            Conv2d(
-                in_channels=new_space_dim,
-                out_channels=out_channels[0],
-                kernel_size=1,
-                bias=bias,
+                initializer=initializer,
+                **kwargs,
                 **factory_kwargs
             ),
+            Sequential(
+                Conv2d(
+                    in_channels=d_model,
+                    out_channels=d_model,
+                    kernel_size=3,
+                    stride=1,
+                    padding=(1, 1),
+                    groups=d_model,
+                    bias=bias,
+                    **factory_kwargs,
+                ),
+                ReLU(),
+                Conv2d(
+                    in_channels=d_model,
+                    out_channels=out_channels[0],
+                    kernel_size=1,
+                    bias=bias,
+                    **factory_kwargs
+                ),
+            )
         ])
-        
-        # drivable head 
-        self.drivable_head.extend([
-            # output conv
-            Conv2d(
-                in_channels=new_space_dim,
-                out_channels=new_space_dim,
-                kernel_size=3,
-                stride=1,
-                padding=(1, 1),
-                groups=new_space_dim,
+        self.drivable_head = ModuleList([
+            UpsampleHead(
+                d_model=d_model,
+                expansion_factor=expansion_factor,
+                dropout_p=dropout_p,
+                norm_num_groups=norm_num_groups,
                 bias=bias,
+                initializer=initializer,
+                **kwargs,
                 **factory_kwargs
             ),
-            ReLU(),
-            Conv2d(
-                in_channels=new_space_dim,
-                out_channels=out_channels[1],
-                kernel_size=1,
-                bias=bias,
-                **factory_kwargs
-            ),  
+            Sequential(
+                Conv2d(
+                    in_channels=d_model,
+                    out_channels=d_model,
+                    kernel_size=3,
+                    stride=1,
+                    padding=(1, 1),
+                    groups=d_model,
+                    bias=bias,
+                    **factory_kwargs,
+                ),
+                ReLU(),
+                Conv2d(
+                    in_channels=d_model,
+                    out_channels=out_channels[1],
+                    kernel_size=1,
+                    bias=bias,
+                    **factory_kwargs
+                ),
+            )
         ])
 
         self._reset_parameters()
     
     
     def _reset_parameters(self,) -> None:
-        for layer in self.lane_head:
+        for layer in self.lane_head[-1]:
             if isinstance(layer, Conv2d):
                 self.initializer(layer.weight)
                 if layer.bias is not None:
                     zeros_(layer.bias)
 
-        for layer in self.drivable_head:
+        for layer in self.drivable_head[-1]:
             if isinstance(layer, Conv2d):
                 self.initializer(layer.weight)
                 if layer.bias is not None:
                     zeros_(layer.bias)
                 
 
-    def forward(self, input: Tensor) -> Tuple[Tensor]:
-        line_mask = self.lane_head.forward(input)
-        drivable_mask = self.drivable_head.forward(input)
+    def forward(
+        self, 
+        input: Tensor, 
+        outputs_stem: Tensor
+    ) -> Tuple[Tensor]:
+        line_head_feat = self.lane_head[0](input, outputs_stem)
+        line_mask = self.lane_head[-1](line_head_feat)
+        
+        drivable_head_feat = self.drivable_head[0](input, outputs_stem)
+        drivable_mask = self.drivable_head[-1](drivable_head_feat)
+        
         return (drivable_mask, line_mask)
 
 
