fda8d795c3d2e8002a98ef8d96e61683b3e7d0fb
Phuoc Thinh <thinhnguyen.7062@gmail.com>
2024-11-13 15:53:45 +0700

    split the segmentation head class to drivable-lane head class and normal head class.

--- model/seg_head.py
+++ model/seg_head.py
@@ -26,7 +26,6 @@ class UpsampleHead(Module):
     def __init__(
         self,  
         d_model: int = 64,
-        expansion_factor: float = 3,
         dropout_p: float = 0.1,
         norm_num_groups: int = 4,
         bias: bool = True, 
@@ -39,7 +38,7 @@ class UpsampleHead(Module):
         Upsample Head is the module that restore the resolution similar to the original input.
 
         Args:
-            d_model (int, optional): Dimentions / Number of feature maps of the whole model. 
+            d_model (int, optional): Dimensions / Number of feature maps of the whole model. 
                 Defaults to 64.
             expansion_factor (float, optional): Expansion factor in Inverted Residual block. 
                 Defaults to 3.
@@ -56,7 +55,6 @@ class UpsampleHead(Module):
         factory_kwargs = {"device": device, "dtype": dtype}
         decoder_layer_kwargs = {
             "in_channels": d_model,
-            "expansion_factor": expansion_factor,
             "dropout_p": dropout_p,
             "norm_num_groups": norm_num_groups,
             "bias": bias,
@@ -137,15 +135,14 @@ class UpsampleHead(Module):
         Z = self.layers[-1][0](Z) # just upsample
         
         return Z
-        
-
-class SegmentationHead(Module):
+ 
+ 
+class SegHead(Module):
     def __init__(
         self, 
         d_model: int,
-        expansion_factor: float = 3,
         dropout_p: float = 0.1,
-        out_channels: int | Tuple[int, int] = [2, 2],
+        out_channels: int = 2,
         norm_num_groups: int = 4,
         bias: bool = True, 
         initializer: str | Callable[[Tensor], Tensor] = "he_uniform",
@@ -154,11 +151,10 @@ class SegmentationHead(Module):
         **kwargs,
     ) -> None:
         r"""
-        Upsample Head is the module that generate segmentation mask for Line and Drivable area
-        Segmentation task.
+        SegHead is the module that generate segmentation mask for Segmentation task.
 
         Args:
-            d_model (int): Dimentions / Number of feature maps of the whole model. 
+            d_model (int): Dimensions / Number of feature maps of the whole model. 
             expansion_factor (float, optional): Expansion factor in Inverted Residual block.
                 Defaults to 3.
             dropout_p (float, optional): Dropout probability. Defaults to 0.1.
@@ -177,9 +173,8 @@ class SegmentationHead(Module):
         d_new = max(16, d_model//4)
         initializer = _get_initializer(initializer)
         self.initializer = initializer
-        out_channels = _pair(out_channels)
         
-        self.lane_head = ModuleList([
+        self.seg_head = ModuleList([
             Conv2d(
                 in_channels=d_model,
                 out_channels=d_new,
@@ -189,7 +184,6 @@ class SegmentationHead(Module):
             ),
             UpsampleHead(
                 d_model=d_new,
-                expansion_factor=expansion_factor,
                 dropout_p=dropout_p,
                 norm_num_groups=norm_num_groups,
                 bias=bias,
@@ -211,46 +205,7 @@ class SegmentationHead(Module):
                 ReLU(),
                 Conv2d(
                     in_channels=d_new,
-                    out_channels=out_channels[0],
-                    kernel_size=1,
-                    bias=bias,
-                    **factory_kwargs
-                ),
-            )
-        ])
-        self.drivable_head = ModuleList([
-            Conv2d(
-                in_channels=d_model,
-                out_channels=d_new,
-                kernel_size=1,
-                bias=bias,
-                **factory_kwargs    
-            ),
-            UpsampleHead(
-                d_model=d_new,
-                expansion_factor=expansion_factor,
-                dropout_p=dropout_p,
-                norm_num_groups=norm_num_groups,
-                bias=bias,
-                initializer=initializer,
-                **kwargs,
-                **factory_kwargs
-            ),
-            Sequential(
-                Conv2d(
-                    in_channels=d_new,
-                    out_channels=d_new,
-                    kernel_size=3,
-                    stride=1,
-                    padding=(1, 1),
-                    groups=d_new,
-                    bias=bias,
-                    **factory_kwargs,
-                ),
-                ReLU(),
-                Conv2d(
-                    in_channels=d_new,
-                    out_channels=out_channels[1],
+                    out_channels=out_channels,
                     kernel_size=1,
                     bias=bias,
                     **factory_kwargs
@@ -262,21 +217,11 @@ class SegmentationHead(Module):
     
     
     def _reset_parameters(self,) -> None:
-        self.initializer(self.lane_head[0].weight)
-        if self.lane_head[0].bias is not None:
-            zeros_(self.lane_head[0].bias)
+        self.initializer(self.seg_head[0].weight)
+        if self.seg_head[0].bias is not None:
+            zeros_(self.seg_head[0].bias)
         
-        self.initializer(self.drivable_head[0].weight)
-        if self.drivable_head[0].bias is not None:
-            zeros_(self.drivable_head[0].bias)
-        
-        for layer in self.lane_head[-1]:
-            if isinstance(layer, Conv2d):
-                self.initializer(layer.weight)
-                if layer.bias is not None:
-                    zeros_(layer.bias)
-
-        for layer in self.drivable_head[-1]:
+        for layer in self.seg_head[-1]:
             if isinstance(layer, Conv2d):
                 self.initializer(layer.weight)
                 if layer.bias is not None:
@@ -287,16 +232,93 @@ class SegmentationHead(Module):
         self, 
         input: Tensor, 
         outputs_stem: Tensor
+    ) -> Tensor:
+        r"""
+        Forward method of Segmentation Head
+
+        Args:
+            input (Tensor): Ouput of decoder
+            outputs_stem (Tensor): Outputs of stem blocks
+
+        Returns:
+            Tuple[Tensor]: Segmentation mask
+        """
+        seg_head_feat = self.seg_head[0](input)
+        seg_head_feat = self.seg_head[1](seg_head_feat, outputs_stem)
+        seg_mask = self.seg_head[-1](seg_head_feat)
+
+        return seg_mask
+        
+
+class DrivableAndLaneSegHead(Module):
+    def __init__(
+        self, 
+        d_model: int,
+        dropout_p: float = 0.1,
+        out_channels: int | Tuple[int, int] = [2, 2],
+        norm_num_groups: int = 4,
+        bias: bool = True, 
+        initializer: str | Callable[[Tensor], Tensor] = "he_uniform",
+        device=None,
+        dtype=None,
+        **kwargs,
+    ) -> None:
+        r"""
+        LineAndDrivableSegHead is the module that generate segmentation masks for Line and 
+        Drivable area Segmentation task.
+
+        Args:
+            d_model (int): Dimensions / Number of feature maps of the whole model. 
+            expansion_factor (float, optional): Expansion factor in Inverted Residual block.
+                Defaults to 3.
+            dropout_p (float, optional): Dropout probability. Defaults to 0.1.
+            out_channels (int | Tuple[int, int], optional): Number of channels of output massk. 
+                Defaults to (2, 2).
+            norm_num_groups (int, optional): Control number of groups to be normalized. If
+                ``norm_num_groups`` = 1, this is equivalent to LayerNorm. Defaults to 4.
+            bias (bool, optional): If ``True``, add trainable bias to building blocks. Defaults to 
+                True.
+            initializer (str | Callable[[Tensor], Tensor], optional): Parameters initializer.
+                Defaults to "he_uniform" aka _kaiming_uniform.
+        """
+        super().__init__()
+        
+        out_channels = _pair(out_channels)
+        kwargs = {
+            "d_model": d_model,
+            "dropout_p": dropout_p,
+            "norm_num_groups": norm_num_groups,
+            "bias": bias,
+            "initializer": initializer,
+            "device": device,
+            "dtype": dtype,
+            **kwargs
+        }
+        
+        # drivable head and lane head are segmentation heads
+        self.drivable_head = SegHead(out_channels=out_channels[0], **kwargs)
+        self.lane_head = SegHead(out_channels=out_channels[1], **kwargs)
+                
+
+    def forward(
+        self, 
+        input: Tensor, 
+        outputs_stem: Tensor
     ) -> Tuple[Tensor]:
-        line_head_feat = self.lane_head[0](input)
-        line_head_feat = self.lane_head[1](line_head_feat, outputs_stem)
-        line_mask = self.lane_head[-1](line_head_feat)
+        r"""
+        Forward method of Line and Drivable Segmentations
+
+        Args:
+            input (Tensor): Output of decoder
+            outputs_stem (Tensor): Outputs of stem block 
+
+        Returns:
+            Tuple[Tensor]: Drivable mask and Line mask
+        """
+        drivable_mask = self.drivable_head(input, outputs_stem)
+        lane_mask = self.lane_head(input, outputs_stem)
         
-        drivable_head_feat = self.drivable_head[0](input)
-        drivable_head_feat = self.drivable_head[1](drivable_head_feat, outputs_stem)
-        drivable_mask = self.drivable_head[-1](drivable_head_feat)
-        
-        return (drivable_mask, line_mask)
+        return (drivable_mask, lane_mask)
 
 
 if __name__ == "__main__":
@@ -304,7 +326,7 @@ if __name__ == "__main__":
     from torchinfo import summary
     from fvcore import nn as fnn
 
-    model = SegmentationHead(
+    model = DrivableAndLaneSegHead(
         in_channels= 48,
         out_channels= 2
     )
