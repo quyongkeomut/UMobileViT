05a9c022d924c90b2574fd5aa032444c91d4bbe4
bangthe2222 <bangthedz2222@gmail.com>
2024-12-13 19:19:49 +0700

    fix colorJiter

--- augmentation/augmentation.py
+++ augmentation/augmentation.py
@@ -9,24 +9,32 @@ _VALID_NORM = v2.Compose([
 
 _NO_MASK_AUG = v2.Compose([
     v2.RandomGrayscale(),
-    v2.ColorJitter(brightness=0.2,
-                    contrast=0.1,
-                    saturation=0.1,
-                    hue=0.1),
     v2.RandomChoice([
-        v2.GaussianNoise(sigma=0.01),
-        v2.GaussianBlur(kernel_size=5),
-        v2.RandomAdjustSharpness(sharpness_factor=2)
+        v2.ColorJitter(brightness=0.25,
+                            contrast=0.25,
+                            saturation=0.25,
+                            hue=0.25),
+        v2.Identity(),
     ]),
-    v2.RandomErasing(),
+    
+    v2.RandomChoice([
+        v2.GaussianNoise(sigma=0.1),
+        v2.GaussianBlur(kernel_size=5),
+        v2.RandomAdjustSharpness(sharpness_factor=2),
+        v2.Identity()
+    ]),
+    # v2.RandomEqualize()
     # v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
 _WITH_MASK_AUG = v2.Compose([
     # v2.RandomResizedCrop(size=512, scale=(0.9, 1.0)),
     v2.RandomHorizontalFlip(p=0.5),
-    v2.RandomRotation(degrees=30),
+    v2.RandomRotation(degrees=25),
+    v2.RandomErasing(scale = (0.02, 0.02)),
 ])
+
+
 
 
 class CustomAug:
