3a1c2b5da16d9aae7e465b4db6ad9e47a4f310f2
Phuoc Thinh <thinhnguyen.7062@gmail.com>
2025-01-02 12:39:15 +0700

    include padding to mask

--- datasets/pascal_datasets.py
+++ datasets/pascal_datasets.py
@@ -49,12 +49,15 @@ def add_padding(image):
     if h > w:
         # Calculate the padding for width
         pad_width = (h - w) // 2
-        padded_image = np.pad(image, ((0, 0), (pad_width, pad_width), (0, 0)), mode='constant', constant_values=0)
+        pad = ((0, 0), (pad_width, pad_width))
     else:
         # Calculate the padding for height
         pad_height = (w - h) // 2
-        padded_image = np.pad(image, ((pad_height, pad_height), (0, 0), (0, 0)), mode='constant', constant_values=0)
-    
+        pad = ((pad_height, pad_height), (0, 0))
+        
+    if len(image.shape) == 3:
+        pad += ((0, 0), )    
+    padded_image = np.pad(image, pad, mode='constant', constant_values=0)
     return padded_image
 
 class VOC2012Dataset(torch.utils.data.Dataset):
@@ -99,6 +102,7 @@ class VOC2012Dataset(torch.utils.data.Dataset):
         image = add_padding(image)
 
         label = cv2.imread(label_path, cv2.IMREAD_GRAYSCALE)
+        label = add_padding(label)
 
         image = cv2.resize(image, self.size, interpolation=cv2.INTER_NEAREST)
         label = cv2.resize(label, self.size, interpolation=cv2.INTER_NEAREST)
